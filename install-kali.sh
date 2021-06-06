04a28c0ac7d52e433183744aad0c2d39bde205f5
SILENCER <82955298+hk4crprasad@users.noreply.github.com>
2021-06-06 08:49:51 +0530

    Update install-kali.sh

--- install-kali.sh
+++ install-kali.sh
@@ -1,5 +1,5 @@
 #install-kali by Strix cyber community
-
+folder=kali-fs
 #colours
 cyan='\033[1;36m'
 green='\033[1;32m'
@@ -124,4 +124,48 @@ echo " "
 clear
 rm dekal.txt
 echo " "
+cat > $folder/root/.bash_profile <- EOM
+#!/bin/bash
+echo -e "\e[32m[\e[31m*\e[32m] \e[34minstalling"
+gpg --keyserver hkp://keys.gnupg.net --recv-key 7D8D0BF6
+gpg --fingerprint 7D8D0BF6
+gpg -a --export 7D8D0BF6 | apt-key add -
+apt update
+apt-get update
+apt-get install xfce4 xfce4-terminal tightvncserver -y
+apt-get install xfe -y
+apt-get clean
+mkdir ~/.vnc
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Xfce4/xstartup -P ~/.vnc/
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Xfce4/vncserver-start -P /usr/local/bin/
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/DesktopEnvironment/Apt/Xfce4/vncserver-stop -P /usr/local/bin/
+chmod +x ~/.vnc/xstartup
+chmod +x /usr/local/bin/vncserver-start
+chmod +x /usr/local/bin/vncserver-stop
+echo " "
+echo "You can now start vncserver by running vncserver-start"
+echo " "
+echo "It will ask you to enter a password when first time starting it."
+echo " "
+echo "The VNC Server will be started at 127.0.0.1:5901"
+echo " "
+echo "You can connect to this address with a VNC Viewer you prefer"
+echo " "
+echo "Connect to this address will open a window with Xfce4 Desktop Environment"
+echo " "
+echo " "
+echo " "
+echo "Running vncserver-start"
+echo " "
+echo " "
+echo " "
+echo "To Kill VNC Server just run vncserver-stop"
+echo " "
+echo " "
+echo " "
+echo "export DISPLAY=":1"" >> /etc/profile
+source /etc/profile
+vncserver-start
+rm -rf /root/.bash_profile
+EOM
 ./start-kali.sh
